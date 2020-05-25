83689cc8e9ecd766844d5110aeef0b563275ca41
christine <bumpouce@gmail.com>
2020-05-25 15:54:25 -0700

    Import merge fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment } from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Container, Jumbotron } from 'react-bootstrap'
 
@@ -11,7 +11,6 @@ import Leaderboard from './component/Leaderboard';
 import UserHome from './component/UserHome'
 import About from './component/About'
 import Game from './component/Game'
-import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
 
 
 
