329e2b9b115fc4061d06fdf52cf0b61637ad673f
github username <github email address>
2020-05-26 16:17:26 -0700

    sylvia

--- src/component/UserHome.js
+++ src/component/UserHome.js
@@ -2,51 +2,57 @@ import React, { Component } from 'react'
 import AuthHOC from '../HOC/AuthHOC'
 
 export class UserHome extends Component {
-    render() {
-		const name=this.props.user ? this.props.user.username : "player"
-        return (
-<div className="container">
-	<section id="our-stats">
-		<div className="row mb-5">
-			<div className="col text-center">
-				<h2 className="text-green h1 text-center">{`Welcome ${name}!`}</h2>
-				<p className="text-uppercase font-italic font-weight-light">Remember to play at least 10 minutes a day to build your memory!</p>
-			</div>
-		</div>
-		<div className="row text-center">
-			<div className="col">
-					<div className="counter">
-						<i className="fa fa-hourglass-o fa-2x text-green"></i>
-						<h2 className="timer count-title count-number" data-to="100" data-speed="1500">100</h2>
-						<p className="count-text ">Highscore (Easy)</p>
+	render() {
+		const name = this.props.user ? this.props.user.username : "Player"
+		return (
+			<div className="container">
+				<section id="our-stats">
+
+					<div className="row mb-5">
+						<div className="col text-center">
+							<h2 className="text-green h1 text-center">{`Welcome ${name}!`}</h2>
+							<p className="text-uppercase font-italic font-weight-light">Remember to play at least 10 minutes a day to build your memory!</p>
+						</div>
 					</div>
-			</div>
-			<div className="col">
-					<div className="counter">
-						<i className="fa fa-hourglass-half fa-2x text-green"></i>
-						<h2 className="timer count-title count-number" data-to="1700" data-speed="1500">1,700</h2>
-						<p className="count-text ">Highscore (Medium)</p>
+					<div className="row mb-5">
+				<div className="col text-center">
+				<img src={this.props.user.avatar} className="rounded-circle img-fluid" alt="" />
+				</div>
+					</div> 
+					<div className="row text-center">
+						<div className="col">
+							<div className="counter">
+								<i className="fa fa-hourglass-o fa-2x text-green"></i>
+								<h2 className="timer count-title count-number" data-to="100" data-speed="1500">100</h2>
+								<p className="count-text ">Highscore (Easy)</p>
+							</div>
+						</div>
+						<div className="col">
+							<div className="counter">
+								<i className="fa fa-hourglass-half fa-2x text-green"></i>
+								<h2 className="timer count-title count-number" data-to="1700" data-speed="1500">1,700</h2>
+								<p className="count-text ">Highscore (Medium)</p>
+							</div>
+						</div>
+						<div className="col">
+							<div className="counter">
+								<i className="fa fa-hourglass fa-2x text-green"></i>
+								<h2 className="timer count-title count-number" data-to="11900" data-speed="1500">11,900</h2>
+								<p className="count-text ">Highscore (Hard)</p>
+							</div>
+						</div>
+						<div className="col">
+							<div className="counter">
+								<i className="fa fa-clock-o fa-2x text-green"></i>
+								<h2 className="timer count-title count-number" data-to="157" data-speed="1500">157</h2>
+								<p className="count-text ">Total Time Played</p>
+							</div>
+						</div>
 					</div>
+				</section>
 			</div>
-			<div className="col">
-					<div className="counter">
-						<i className="fa fa-hourglass fa-2x text-green"></i>
-						<h2 className="timer count-title count-number" data-to="11900" data-speed="1500">11,900</h2>
-						<p className="count-text ">Highscore (Hard)</p>
-					</div>
-			</div>
-			<div className="col">
-					<div className="counter">
-						<i className="fa fa-clock-o fa-2x text-green"></i>
-						<h2 className="timer count-title count-number" data-to="157" data-speed="1500">157</h2>
-						<p className="count-text ">Total Time Played</p>
-					</div>
-			</div>
-		</div>
-	</section>
-</div>
-        )
-    }
+		)
+	}
 }
 
 export default AuthHOC(UserHome)
